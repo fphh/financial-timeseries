a64e5efa178fd6fcc6cafeba7a330e3b3800c114
Dr. Heinrich Hördegen <hoerdegen@funktional.info>
2019-11-17 14:03:18 +0100

    parse LoggerData

--- src/FinancialTimeseries/Trade/LoggerData.hs
+++ src/FinancialTimeseries/Trade/LoggerData.hs
@@ -11,6 +11,9 @@ import FinancialTimeseries.Trade.Account (Account(..))
 import FinancialTimeseries.Type.Types (Price(..))
 
 
+import Debug.Trace (trace)
+
+
 data LoggerData a = LoggerData {
   ltime :: UTCTime
   , lbaseCurrencyUSDT :: Maybe (Price a)
@@ -18,11 +21,38 @@ data LoggerData a = LoggerData {
   , laccount :: Account a
   } deriving (Show, Read)
 
+notAvailable :: String
+notAvailable = "n/a"
+
 logger ::
   (Show a) =>
   Sys.Handle -> LoggerData a -> IO ()
 logger hd (LoggerData t bcusdt (Price cp) (Account bc qc)) =
-  let bcl = maybe [] ((:[]) . show . unPrice) bcusdt
-
+  let bcl = maybe [notAvailable] ((:[]) . show . unPrice) bcusdt
       str = List.intercalate "," (show t : bcl ++ [show cp, show bc, show qc])
   in Sys.hPutStrLn hd str
+
+parse ::
+  (Read a) =>
+  FilePath -> IO [LoggerData a]
+parse file = do
+  txt <- readFile file
+  let ls = lines txt
+      f [t, bcusdt, cp, bc, qc] =
+        let acnt = Account (read bc) (read qc)
+            usdt =
+              case bcusdt of
+                x | x == notAvailable -> Nothing
+                x -> Just (Price (read x))
+        in LoggerData (read t) usdt (Price (read cp)) acnt
+
+      brk xs =
+        case break (==',') xs of
+          ("", _) -> []
+          (as, bs) ->
+            as : case bs of
+                   [] -> []
+                   _:cs   -> brk cs
+                             
+  return (map (f . brk) ls)
+  
